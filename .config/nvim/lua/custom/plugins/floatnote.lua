7ce6f7002302351658ed8e9bb3a2a31e121886a5
elbunuelo <mail@nicolasarias.co>
2025-01-09 21:04:25 -0500

    Load floatnote from disk if available

--- .config/nvim/lua/custom/plugins/floatnote.lua
+++ .config/nvim/lua/custom/plugins/floatnote.lua
@@ -1,6 +1,14 @@
+local PLUGIN_DIR = '~/Projects/floatnote.nvim/'
+
+if (vim.uv or vim.lop).fs_stat(PLUGIN_DIR) then
+  return {
+    { dir = PLGUIN_DIR },
+  }
+end
+
 return {
   'elbunuelo/floatnote.nvim',
   dependencies = {
     'zk-org/zk-nvim',
-  }
+  },
 }
