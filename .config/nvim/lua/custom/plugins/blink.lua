2b2ae7670a8a963ebf9dbe7a378d98dfca5aecde
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-01-21 08:23:30 -0500

    Remove copilot

--- .config/nvim/lua/custom/plugins/blink.lua
+++ .config/nvim/lua/custom/plugins/blink.lua
@@ -7,8 +7,8 @@ return {
       config = function()
         local luasnip = require 'luasnip'
         luasnip.config.setup {}
-      end
-    }
+      end,
+    },
   },
   version = '*',
   opts = {
@@ -22,61 +22,11 @@ return {
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono',
-      kind_icons = {
-        Copilot = "",
-        Text = '󰉿',
-        Method = '󰊕',
-        Function = '󰊕',
-        Constructor = '󰒓',
-
-        Field = '󰜢',
-        Variable = '󰆦',
-        Property = '󰖷',
-
-        Class = '󱡠',
-        Interface = '󱡠',
-        Struct = '󱡠',
-        Module = '󰅩',
-
-        Unit = '󰪚',
-        Value = '󰦨',
-        Enum = '󰦨',
-        EnumMember = '󰦨',
-
-        Keyword = '󰻾',
-        Constant = '󰏿',
-
-        Snippet = '󱄽',
-        Color = '󰏘',
-        File = '󰈔',
-        Reference = '󰬲',
-        Folder = '󰉋',
-        Event = '󱐋',
-        Operator = '󰪚',
-        TypeParameter = '󰬛',
-      },
     },
     snippets = { preset = 'luasnip' },
     sources = {
-      default = { 'lsp', 'path', 'buffer', 'copilot' },
-      providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-cmp-copilot",
-          score_offset = 100,
-          async = true,
-          transform_items = function(_, items)
-            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-            local kind_idx = #CompletionItemKind + 1
-            CompletionItemKind[kind_idx] = "Copilot"
-            for _, item in ipairs(items) do
-              item.kind = kind_idx
-            end
-            return items
-          end,
-        },
-      },
+      default = { 'lsp', 'path', 'buffer' },
     },
   },
-  opts_extend = { "sources.default" }
+  opts_extend = { 'sources.default' },
 }
