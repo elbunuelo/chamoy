d7bfa7e75fc8c8c75dfb5d405b983dcd7455dcbb
elbunuelo <mail@nicolasarias.co>
2025-01-25 14:33:06 -0500

    Add training wheels config

--- .config/nvim/lua/custom/plugins/training-wheels.lua
+++ .config/nvim/lua/custom/plugins/training-wheels.lua
@@ -2,7 +2,13 @@ local PLUGIN_DIR = os.getenv 'HOME' .. '/Projects/training-wheels.nvim'
 
 if (vim.uv or vim.lop).fs_stat(PLUGIN_DIR) then
   return {
-    { dir = PLUGIN_DIR },
+    {
+      dir = PLUGIN_DIR,
+      opts = {
+        wheels_path = os.getenv 'HOME' .. 'Projects/wheels',
+        default_wheel = '01',
+      },
+    },
   }
 end
 
