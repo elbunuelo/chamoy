bd0bc3fdacb0dbc414b1421e9fe0cf8b884ef954
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-02-03 10:55:43 -0500

    Wheels config

--- .config/nvim/lua/custom/plugins/training-wheels.lua
+++ .config/nvim/lua/custom/plugins/training-wheels.lua
@@ -1,17 +1,20 @@
 local PLUGIN_DIR = os.getenv 'HOME' .. '/Projects/training-wheels.nvim'
 
+local wheels_config = {
+  wheels_path = os.getenv 'HOME' .. '/Projects/wheels',
+  default_wheel = '01',
+}
+
 if (vim.uv or vim.lop).fs_stat(PLUGIN_DIR) then
   return {
     {
       dir = PLUGIN_DIR,
-      opts = {
-        wheels_path = os.getenv 'HOME' .. '/Projects/wheels',
-        default_wheel = '01',
-      },
+      opts = wheels_config,
     },
   }
 end
 
 return {
   'elbunuelo/training-wheels',
+  opts = wheels_config,
 }
