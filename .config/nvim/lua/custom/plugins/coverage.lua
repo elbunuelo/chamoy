59d95817b75c8fee1a5910aef6c58ceddfc67751
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-02-26 11:14:27 -0500

    Add coverage file for editor v2

--- .config/nvim/lua/custom/plugins/coverage.lua
+++ .config/nvim/lua/custom/plugins/coverage.lua
@@ -1,9 +1,11 @@
 return {
-  "andythigpen/nvim-coverage",
-  version = "*",
+  'andythigpen/nvim-coverage',
+  version = '*',
   config = function()
-    require("coverage").setup({
+    require('coverage').setup {
+      commands = true,
       auto_reload = true,
-    })
+      lcov_file = 'packages/aha-editor-v2/editor_js/lcov.info',
+    }
   end,
 }
