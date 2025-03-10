49d1f170ba3ae9ced1ba0ed64511422906d77c3a
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-03-10 11:42:33 -0500

    Misc changes

--- dot_config/nvim/lua/custom/plugins/octo-nvim.lua
+++ dot_config/nvim/lua/custom/plugins/octo-nvim.lua
@@ -2,32 +2,15 @@
 -- The plugin interacts with the github gh cli from nvim.
 
 -- Will use the o prefix to interact with the plugin via keymaps
-vim.api.nvim_set_keymap('n', '<leader>op', '<cmd>:Octo pr search review-requested:elbunuelo state:open<cr>',
-  { desc = 'Search PRs awaiting review from me.' })
-vim.api.nvim_set_keymap('n', '<leader>os', '<cmd>:Octo pr search<cr>',
-  { desc = 'Search all PRs' })
-vim.api.nvim_set_keymap('n', '<leader>om', '<cmd>:Octo pr search author:elbunuelo<cr>',
-  { desc = 'Search my PRs' })
+vim.api.nvim_set_keymap('n', '<leader>op', '<cmd>:Octo pr search review-requested:elbunuelo state:open<cr>', { desc = 'Search PRs awaiting review from me.' })
+vim.api.nvim_set_keymap('n', '<leader>os', '<cmd>:Octo pr search is:open<cr>', { desc = 'Search open PRs' })
+vim.api.nvim_set_keymap('n', '<leader>om', '<cmd>:Octo pr search author:elbunuelo<cr>', { desc = 'Search my PRs' })
 
--- Disabling for now, using default keymaps
--- vim.api.nvim_set_keymap('n', '<leader>oc', '<cmd>:Octo pr checkout<cr>',
---   { desc = 'Check out currently selected PR.' })
---
--- -- ORI is from Octo review init.
--- vim.api.nvim_set_keymap('n', '<leader>ori', '<cmd>:Octo pr review<cr>',
---   { desc = 'Start a review on the currently selected PR.' })
---
--- vim.api.nvim_set_keymap('n', '<leader>orr', '<cmd>:Octo pr review<cr>',
---   { desc = 'Resume a review on the currently selected PR.' })
---
--- vim.api.nvim_set_keymap('n', '<leader>orc', '<cmd>:Octo review comments<cr>',
---   { desc = 'Reveiw pending comments in current review.' })
---
--- vim.api.nvim_set_keymap('n', '<leader>ov', '<cmd>:Octo pr changes<cr>',
---   { desc = 'View changes in current pr' })
---
-vim.api.nvim_set_keymap('n', '<leader>ors', '<cmd>:Octo review submit<cr>',
-  { desc = 'Submit current review.' })
+vim.api.nvim_set_keymap('n', '<leader>oc', '<cmd>:Octo pr checkout<cr>', { desc = 'Check out currently selected PR.' })
+vim.api.nvim_set_keymap('n', '<leader>ori', '<cmd>:Octo review<cr>', { desc = 'Review the currently selected PR.' })
+vim.api.nvim_set_keymap('n', '<leader>orc', '<cmd>:Octo review comments<cr>', { desc = 'Reveiw pending comments in current review.' })
+vim.api.nvim_set_keymap('n', '<leader>ov', '<cmd>:Octo pr changes<cr>', { desc = 'View changes in current pr' })
+vim.api.nvim_set_keymap('n', '<leader>ors', '<cmd>:Octo review submit<cr>', { desc = 'Submit current review.' })
 
 -- <space>ca adds a comment
 -- <space>sa adds a suggestion
@@ -40,18 +23,15 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require "octo".setup(
-      {
-        suppress_missing_scope = {
-          projects_v2 = true,
+    require('octo').setup {
+      suppress_missing_scope = {
+        projects_v2 = true,
+      },
+      mappings = {
+        submit_win = {
+          approve_review = { lhs = '<C-s>', desc = 'approve review' },
         },
-        mappings = {
-          submit_win = {
-            approve_review = { lhs = "<C-s>", desc = "approve review" },
-          }
-        }
-      }
-    )
-  end
-
+      },
+    }
+  end,
 }
