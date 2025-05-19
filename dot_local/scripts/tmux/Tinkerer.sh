ff2932120bc0653713ba482147f1b7c491ddcab5
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-05-19 08:40:42 -0500

    Update tmuxline

--- dot_local/scripts/tmux/Tinkerer.sh
+++ dot_local/scripts/tmux/Tinkerer.sh
@@ -3,4 +3,4 @@
 cd "$(dirname "$0")"
 github=$(. ./github-prs.sh)
 zendesk=$(. ./zendesk.sh)
-echo "#[fg=#9e9e9e,bg=#303030] $github  $zendesk "
+echo "#[bg=#121212,fg=#dca561]#[fg=#121212,bg=#dca561] $github  $zendesk #[fg=#7e9cd8,bg=#dca561]"
