9f112ef78c06675a148ea1cf7cd276405727f740
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-06-10 09:39:09 -0500

    chore: Auto-commit changes to 1 files

--- dot_local/scripts/tamal/constants.rb
+++ dot_local/scripts/tamal/constants.rb
@@ -1,4 +1,4 @@
-EDITOR = (ENV['EDITOR'] || 'vi').freeze
+EDITOR = (ENV['EDITOR'] || 'neovim -c "set laststatus=0" -c "set nonumber" -c "set textwidth=120"').freeze
 NOTES_DIRECTORY = (ENV['NOTES_DIR'] || "#{ENV['HOME']}/notes").freeze
 TEMPLATES_DIRECTORY = (ENV['TEMPLATES_DIR'] || "#{__dir__}/templates").freeze
 MUSTACHE_REGEX = /{{\s*(?<mustache>.*)\s*}}/
