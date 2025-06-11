ac7c1c566119043a70c8721e50726df11da8df55
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-06-11 17:00:11 -0500

    Add Chamoy command

--- dot_config/nvim/lua/custom/plugins/chamoy.lua
+++ dot_config/nvim/lua/custom/plugins/chamoy.lua
@@ -3,8 +3,10 @@
 
 -- Create the Chamoy command that executes !chamoy
 vim.api.nvim_create_user_command('Chamoy', function()
-  vim.cmd('!chamoy')
+  vim.cmd '!chamoy'
 end, { desc = 'Execute chamoy command' })
 
 -- Add normal mode keybinding for <leader>C
 vim.keymap.set('n', '<leader>C', ':Chamoy<CR>', { desc = 'Execute chamoy command', silent = true })
+
+return {}
