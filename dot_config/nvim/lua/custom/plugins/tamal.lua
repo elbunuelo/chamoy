d75cdcf1f606529f2ad65d6ec0dee4388dc61e1b
elbunuelo <mail@nicolasarias.co>
2025-06-18 20:18:42 -0500

    chore: Auto-commit changes to 6 files

--- dot_config/nvim/lua/custom/plugins/tamal.lua
+++ dot_config/nvim/lua/custom/plugins/tamal.lua
@@ -38,6 +38,8 @@ local tamal_commands = {
   { cmd = 'open', desc = 'Open a note', height = 0, key = 'o', use_telescope = true },
   { cmd = 'add-note', desc = 'Add a note', height = 15, key = 'n', needs_time_block = true },
   { cmd = 'three-p', desc = 'Add a 3P note', height = 15, key = 'p' },
+  { cmd = 'zendesk', desc = 'Open Zendesk ticket note', height = 0, key = 'z', needs_ticket_id = true },
+  { cmd = 'zendesk-note', desc = 'Add note to Zendesk ticket', height = 15, key = 'Z', needs_ticket_id = true, needs_section = true },
 }
 
 -- Helper function to calculate window dimensions and position
