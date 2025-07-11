e863774454dd31de19d1914adde28c2b7151248e
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-07-11 09:09:29 -0500

    Also remove pnpm store when nuking

--- dot_config/fish/functions/nuke.fish
+++ dot_config/fish/functions/nuke.fish
@@ -2,5 +2,5 @@
 
 function nuke
   cd "$HOME/projects/aha-app"
-  wo rm -rf node_modules packages/*/node_modules .turbo-cache
+  wo rm -rf node_modules packages/*/node_modules .turbo-cache /Users/nariasgonzalez/Library/pnpm/store/v3
 end
