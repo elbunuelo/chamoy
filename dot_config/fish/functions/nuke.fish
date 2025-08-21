ca1d13368463c8dd15be66e69a82d87ccd9f798d
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-08-21 09:04:29 -0500

    Update nuke

--- dot_config/fish/functions/nuke.fish
+++ dot_config/fish/functions/nuke.fish
@@ -5,9 +5,9 @@ function nuke
   echo "Removing node modules."
   time rm -rf node_modules
   echo "Removing packages node modules."
-  time rm -rf packages/*/node_modules
+  time rm -rf packages/**/node_modules
   echo "Removing services node modules."
-  time rm -rf packages/*/node_modules
+  time rm -rf packages/**/node_modules
   echo "Removing turbo cache."
   time rm -rf .turbo-cache
   echo "Removing pnpm store."
