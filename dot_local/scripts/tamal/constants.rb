9294ecefb3f59c64a418d4a83571c862ad276ca0
Nico Arias <mail@nicolasarias.co>
2026-02-13 15:28:18 -0500

    Remove tamal

--- dot_local/scripts/tamal/constants.rb
+++
@@ -1,4 +0,0 @@
-EDITOR = (ENV['EDITOR'] || 'vim').freeze
-NOTES_DIRECTORY = (ENV['NOTES_DIR'] || "#{ENV['HOME']}/notes").freeze
-TEMPLATES_DIRECTORY = (ENV['TEMPLATES_DIR'] || "#{__dir__}/templates").freeze
-MUSTACHE_REGEX = /{{\s*(?<mustache>.*)\s*}}/
--- dot_local/scripts/tamal/utils.rb
+++
@@ -1,5 +0,0 @@
-def log(text, level = 'DEBUG', config)
-  return unless config.debug
-
-  puts "[#{level}][#{Time.now}] #{text}"
-end
