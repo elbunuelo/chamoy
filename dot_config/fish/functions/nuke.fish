a7b4ca04a14d3afbd590bb5cc38c6eaf5ea479df
Nico Arias <mail@nicolasarias.co>
2026-03-04 09:18:02 -0500

    Don't time nuke

--- dot_config/fish/functions/nuke.fish
+++ dot_config/fish/functions/nuke.fish
@@ -3,13 +3,13 @@
 function nuke
   cd "$HOME/projects/aha-app"
   echo "Removing node modules."
-  time rm -rf node_modules
+  rm -rf node_modules
   echo "Removing packages node modules."
-  time rm -rf packages/*/node_modules
+  rm -rf packages/*/node_modules
   echo "Removing services node modules."
-  time rm -rf services/*/node_modules
+  rm -rf services/*/node_modules
   echo "Removing turbo cache."
-  time rm -rf .turbo-cache
+  rm -rf .turbo-cache
   echo "Removing pnpm store."
-  time rm -rf /Users/nariasgonzalez/Library/pnpm/store/v3
+  rm -rf /Users/nariasgonzalez/Library/pnpm/store/v3
 end
