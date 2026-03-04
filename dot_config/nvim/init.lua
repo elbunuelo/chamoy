469df7ee2cce0acb11b193b0283b9517dd6dad4b
Nico Arias <mail@nicolasarias.co>
2026-03-04 09:18:06 -0500

    Disable mouse

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -404,7 +404,7 @@ require('lazy').setup({
 vim.wo.number = true
 
 -- Enable mouse mode
--- vim.o.mouse = 'a'
+vim.o.mouse = ''
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
